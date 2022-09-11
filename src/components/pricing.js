a35225722574b13552a9acf22abeb96d000ae6af
IVANDAYAs <64883574+IVANDAYAs@users.noreply.github.com>
2022-09-11 18:48:58 +0530

    video updated

--- src/components/pricing.js
+++ src/components/pricing.js
@@ -3,8 +3,8 @@ import "./pricing.css";
 import pricing from "./images/pricing.JPG";
 import Cards from "./cards.js";
 
-export default function(){
-    return(
+export default function () {
+    return (
         <div id="pricing" className="pricing">
             <div className="ph">
                 <h1>PRICING</h1>
@@ -12,7 +12,7 @@ export default function(){
                     <h3>Be fit without breaking the bank</h3>
                 </div> */}
             </div>
-            
+            {/*             
             <div className="container pmiddle">
                 <div className="row">
                     <div className="col-md-6 pmleft">
@@ -23,20 +23,38 @@ export default function(){
                         <p className="pmrp">24 live classes.<br/>Access to all our pre-recorded classes.<br/>Get a pass to the community-driven programmes.<br/>Get indulged in co-cuuricular activities.<br/>Brand connisure programme.<br/>Subscription to our newsletter.<br/>Nutritionist consultation. </p>
                     </div>
                 </div>
-            </div>
+            </div> */}
 
 
             <div className="container ">
 
                 <div className="row pbottom">
+                    <h4>1 fitness plan/month <br />+<br />1 nutrition plan/month</h4>
                     <div className=" col-lg-4">
-                        <Cards name="GOLD PACK" discount="50% off of 4999" price="2499" />
+                        <Cards name="Best-Value" discount="6 months" price="1999" />
                     </div>
                     <div className=" col-lg-4">
-                        <Cards name="SILVER PACK" discount="30% off of 2499" price="1799" />
+                        <Cards name="Best-Seller" discount="3 months" price="1499" />
                     </div>
                     <div className=" col-lg-4">
-                        <Cards name="RHODIUM PACK" discount="60% off of 9999" price="4499" />
+                        <Cards name="Normal pack" discount="1 month" price="599" />
+                    </div>
+                </div>
+
+            </div>
+
+            <div className="container ">
+
+                <div className="row pbottom">
+                    <h4>1 fitness plan/month <br />+<br />1 nutrition plan/month<br/>+<br/>1 doctor consultation/month</h4>
+                    <div className=" col-lg-4">
+                        <Cards name="Best-Value" discount="6 months" price="4999" />
+                    </div>
+                    <div className=" col-lg-4">
+                        <Cards name="Best-Seller" discount="3 months" price="3499" />
+                    </div>
+                    <div className=" col-lg-4">
+                        <Cards name="Normal pack" discount="1 month" price="1499" />
                     </div>
                 </div>
 
