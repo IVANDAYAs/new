c7dde6ddccf13afff3047c46e7fcdac4f623695b
IVANDAYAs <64883574+IVANDAYAs@users.noreply.github.com>
2022-09-15 12:11:28 +0530

    payment

--- src/components/cards.js
+++ src/components/cards.js
@@ -1,14 +1,28 @@
 import React from "react";
 import "./cards.css";
+import { Helmet } from "react-helmet";
 
-export default function(props){
-    return(
-        <div className="pcard">
+export default function (props) {
+
+
+    document.addEventListener("DOMContentLoaded", function () {
+        const Script = document.createElement("script");
+        //id should be same as given to form element
+        const Form = document.getElementById(props.id);
+        Script.setAttribute('src', 'https://checkout.razorpay.com/v1/payment-button.js');
+        Script.setAttribute('data-payment_button_id', props.payId);
+        Script.async = "true";
+        Form.appendChild(Script);
+    });
+
+
+    return (
+        <div className="pcard"  >
             <div className="oo">
                 <h2><b>{props.name}</b></h2>
                 <b><p>{props.discount}</p></b>
-                <h4>{props.price} RUPEES</h4>
-                <form><script src="https://checkout.razorpay.com/v1/payment-button.js" data-payment_button_id="pl_J7K3uCk9gptwjP" async> </script> </form>
+
+                <form id={props.id}  > </form>
             </div>
         </div>
     );
--- src/components/pricing.js
+++ src/components/pricing.js
@@ -4,26 +4,13 @@ import pricing from "./images/pricing.JPG";
 import Cards from "./cards.js";
 
 export default function () {
+
+      
     return (
         <div id="pricing" className="pricing">
             <div className="ph">
                 <h1>PRICING</h1>
-                {/* <div className="pd">
-                    <h3>Be fit without breaking the bank</h3>
-                </div> */}
             </div>
-            {/*             
-            <div className="container pmiddle">
-                <div className="row">
-                    <div className="col-md-6 pmleft">
-                        <img className="pimg" src={pricing}></img>
-                    </div>
-                    <div className="col-md-6 pmright">
-                        <h2 className="pmrh"><b>GOLD PACK</b> - 2499 Rupees</h2>
-                        <p className="pmrp">24 live classes.<br/>Access to all our pre-recorded classes.<br/>Get a pass to the community-driven programmes.<br/>Get indulged in co-cuuricular activities.<br/>Brand connisure programme.<br/>Subscription to our newsletter.<br/>Nutritionist consultation. </p>
-                    </div>
-                </div>
-            </div> */}
 
 
             <div className="container ">
@@ -31,13 +18,13 @@ export default function () {
                 <div className="row pbottom">
                     <h4>1 fitness plan/month <br />+<br />1 nutrition plan/month</h4>
                     <div className=" col-lg-4">
-                        <Cards name="Best-Value" discount="6 months" price="1999" />
+                        <Cards name="Best-Value" discount="6 months" price="1999" id="donateFormOne" payId = "pl_KI3MYUwLKkxjNI" />
                     </div>
                     <div className=" col-lg-4">
-                        <Cards name="Best-Seller" discount="3 months" price="1499" />
+                        <Cards name="Best-Seller" discount="3 months" price="1499" id="donateFormTwo" payId="pl_KI4scRV3EkzlSh"/>
                     </div>
                     <div className=" col-lg-4">
-                        <Cards name="Normal pack" discount="1 month" price="599" />
+                        <Cards name="Normal pack" discount="1 month" price="599" id="donateFormThree" payId="pl_KI4xRaBTEv2tcF" />
                     </div>
                 </div>
 
@@ -48,13 +35,13 @@ export default function () {
                 <div className="row pbottom">
                     <h4>1 fitness plan/month <br />+<br />1 nutrition plan/month<br/>+<br/>1 doctor consultation/month</h4>
                     <div className=" col-lg-4">
-                        <Cards name="Best-Value" discount="6 months" price="4999" />
+                        <Cards name="Best-Value" discount="6 months" price="4999" id="donateFormFour" payId="pl_KI4z0MEmsXuL9F" />
                     </div>
                     <div className=" col-lg-4">
-                        <Cards name="Best-Seller" discount="3 months" price="3499" />
+                        <Cards name="Best-Seller" discount="3 months" price="3499" id="donateFormFive" payId="pl_KI50GTu7mqY9Rk" />
                     </div>
                     <div className=" col-lg-4">
-                        <Cards name="Normal pack" discount="1 month" price="1499" />
+                        <Cards name="Normal pack" discount="1 month" price="1499" id="donateFormSix" payId="pl_KI51ziY1ETC3hC" />
                     </div>
                 </div>
 
