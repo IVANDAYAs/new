54d4dd500222d893fa62e8cc3ac08dc903c3932f
IVANDAYAs <64883574+IVANDAYAs@users.noreply.github.com>
2022-09-22 11:24:27 +0530

    s

--- src/components/know.js
+++ src/components/know.js
@@ -8,6 +8,10 @@ export default function () {
         <div >
             <div className="knowNav"></div>
             <div className="know">
+                <h1>To get a sneak peak write to us.</h1>
+
+                <a id="instak" href="https://instagram.com/fitnessdietandhealth_?igshid=YmMyMTA2M2Y="> <p className="social" > <i class="col fa-1x fa-brands fa-instagram"></i> Instagram  </p>  </a>
+                <a id="mailk" href="mailto:fitnessdietandhealth.india@gmail.com"> <p className="social" > <i class="col fa-2x fa-solid fa-envelope"></i> Email  </p>  </a>
                 <img src={knowImg}></img>
                 <img src={knowImg1}></img>
             </div>
